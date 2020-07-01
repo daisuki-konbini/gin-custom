3bdca5be81fd976ce8de76fe919c57f9d1b415eb
xucheng <xucheng@gmail.com>
2020-07-01 17:58:21 +0900

    234

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"flag"
 	"gin/server"
+
+	"github.com/gin-gonic/gin"
 )
 
+var env = flag.String("env", "debug", "input the env debug|test|release")
+
 func main() {
+	flag.Parse()
+	gin.SetMode(*env)
+
 	server.Init()
 }
